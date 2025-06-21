ee00702de7aa57e62f19faf08bf94c8dfa573ab2
Nikhil <nikhilbhor.bhor@gmail.com>
2025-06-21 12:02:57 +0530

    Learned installation of dependancies and basic implementation

--- 02 Day 2/backend/server.js
+++ 02 Day 2/backend/server.js
@@ -6,6 +6,10 @@ app.get("/api/notes", (req, res) => {
     res.send("You have created 5 notes.")
 })
 
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({message:"You have created 10 notes."})
+})
+
 app.listen(5001, () => {
     console.log("Server started on PORT:5001")
 });
