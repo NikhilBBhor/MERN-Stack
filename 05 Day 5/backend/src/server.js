4798e15e7c2c05bed93f6164fa60a0c0da2309bc
Nikhil <nikhilbhor.bhor@gmail.com>
2025-06-22 11:57:16 +0530

    Learned about middleware and rate limiting

--- 05 Day 5/backend/src/server.js
+++ 05 Day 5/backend/src/server.js
@@ -2,16 +2,23 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 // middleware
 app.use(express.json());
+app.use(rateLimiter);
+
+// custom simple middleware
+app.use((req, res, next) => {
+    console.log(`Req method is ${req.method} and req URL is ${req.url}`);
+    next();
+});
 
 app.use("/api/notes", notesRoutes);
 
