a339c1a77c60ce1b0600aac762e656c16f10f412
Nikhil <nikhilb.bhor@gmail.com>
2025-06-25 22:57:58 +0530

    Tailwind and daisyui setup

--- 06 Building Notes App/frontend/src/App.jsx
+++ 06 Building Notes App/frontend/src/App.jsx
@@ -6,8 +6,21 @@ import toast from "react-hot-toast"
 
 const App = () => {
   return (
-    <div>
-      <button onClick={() => toast.success("Congrats")}>Click Me</button>
+    <div data-theme='forest'>
+      {/* Pure Tailwind CSS */}
+      <button onClick={() => toast.success("Congrats")}
+        className='text-red-500 p-4 bg-pink-300'>
+        Test Button
+      </button>
+      {/* Daisy UI */}
+      <button className='btn btn-outline'>Daisy UI</button>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/create' element={<CreatePage />} />
