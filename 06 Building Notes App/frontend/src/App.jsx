cd8041112853326a640e25ac5d0986933645c462
Nikhil <nikhilb.bhor@gmail.com>
2025-06-27 07:17:52 +0530

    Created frontend backend connection and learned about CORS

--- 06 Building Notes App/frontend/src/App.jsx
+++ 06 Building Notes App/frontend/src/App.jsx
@@ -8,19 +8,19 @@ const App = () => {
   return (
     <div data-theme='forest'>
       {/* Pure Tailwind CSS */}
-      <button onClick={() => toast.success("Congrats")}
+      {/* <button onClick={() => toast.success("Congrats")}
         className='text-red-500 p-4 bg-pink-300'>
         Test Button
-      </button>
+      </button> */}
       {/* Daisy UI */}
-      <button className='btn btn-outline'>Daisy UI</button>
+      {/* <button className='btn btn-outline'>Daisy UI</button>
       <button className="btn">Button</button>
       <button className="btn btn-neutral">Neutral</button>
       <button className="btn btn-primary">Primary</button>
       <button className="btn btn-secondary">Secondary</button>
       <button className="btn btn-accent">Accent</button>
       <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+      <button className="btn btn-link">Link</button> */}
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/create' element={<CreatePage />} />
