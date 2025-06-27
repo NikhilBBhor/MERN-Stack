54195e8050748e97f4dbeff652ef53621d92d5b2
Nikhil <nikhilb.bhor@gmail.com>
2025-06-27 18:24:03 +0530

    Built homepage and create note page

--- 06 Building Notes App/backend/src/config/upstash.js
+++ 06 Building Notes App/backend/src/config/upstash.js
@@ -7,7 +7,7 @@ dotenv.config()
 // create a rate limiter that allows 100 requests per 60 seconds
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(5, "20 s")
+    limiter: Ratelimit.slidingWindow(10, "20 s")
 });
 
 export default ratelimit;
