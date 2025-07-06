385edd77cc4ec866a7d45e66ca909effb19ea584
Nikhil <nikhilb.bhor@gmail.com>
2025-07-06 10:25:52 +0530

    Project deployment

--- 06 Building Notes App/backend/src/server.js
+++ 06 Building Notes App/backend/src/server.js
@@ -4,19 +4,24 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from 'cors'
+import path from "path"
 
 dotenv.config();
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
-// middleware
-app.use(
-    cors({
-        origin: 'http://localhost:5173'
-    })
-);
+if (process.env.NODE_ENV !== "production") {
+    // middleware
+    app.use(
+        cors({
+            origin: 'http://localhost:5173'
+        })
+    );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -27,6 +32,15 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/notes", notesRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join("../frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     console.log("Server started on PORT:", PORT)
